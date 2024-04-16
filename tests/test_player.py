3e76e2351e975235bb75d9001aca44444d364c83
Victor Martinez <vicplusplus@proton.me>
2024-04-16 19:13:33 -0400

    fix test player

--- tests/test_player.py
+++ tests/test_player.py
@@ -12,14 +12,14 @@ class TestPlayer(unittest.TestCase):
         self.player.hand = [Domino(1, 2), Domino(3, 4)]
         board = Board(6, 4, 7)
         board.add(Domino(1, 5), "right")
-        move = self.player.play(board)
+        move = self.player.select_move(board)
         self.assertEqual(move, Move(Domino(2, 1), "left"))
 
     def test_invalid_move(self):
         self.player.hand = [Domino(1, 2), Domino(3, 4)]
         board = Board(6, 4, 7)
         board.add(Domino(5, 6), "left")
-        move = self.player.play(board)
+        move = self.player.select_move(board)
         self.assertIsNone(move)
 
     def test_score(self):
